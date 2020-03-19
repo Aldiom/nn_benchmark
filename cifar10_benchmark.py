e0a874f69a9336c3193aa93a47d4097a8f814887
Aldiom <alexis.diomedi.12@sansano.usm.cl>
2020-03-19 17:34:57 -0300

    Agregado soporte para saved models (y TRT). Fix de bug con TFLite

--- cifar10_benchmark.py
+++ cifar10_benchmark.py
@@ -1,5 +1,6 @@
-from tensorflow import keras, data, device, lite
+from tensorflow import keras, data, device, lite, saved_model
 from timeit import timeit, repeat
+from os.path import isdir
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -13,15 +14,18 @@ arguments = parser.parse_args()
 def main(args):
 	mod_file = args.mod_file
 	tflite = '.tflite' in mod_file
+	sav_mod = isdir(mod_file)
 	b_sz = args.b_sz
 	if args.cpu:
 		print('Forcing CPU usage')
 		dev = 'device:CPU:0'
-	else:
+	else: #TODO: chequear si hay gpu en sistema
 		dev = 'device:GPU:0'
 
 	if tflite:
 		model = lite.Interpreter(model_path = mod_file)
+	elif sav_mod:
+		model = saved_model.load(mod_file)
 	else:
 		model = keras.models.load_model(mod_file, compile=False)
 
@@ -31,7 +35,7 @@ def main(args):
 	y_ds = data.Dataset.from_tensor_slices(y_test.astype('float32'))
 	eval_ds = data.Dataset.zip((x_ds, y_ds)).batch(64)
 
-	if not tflite:
+	if not tflite and not sav_mod:
 		model.compile(loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 		if args.acc:
@@ -44,7 +48,7 @@ def main(args):
 	if tflite:
 		in_idx = model.get_input_details()[0]['index'] 
 		out_idx = model.get_output_details()[0]['index'] 
-		model.resize_tensor_input(input_idx, [b_sz, 32, 32, 3])
+		model.resize_tensor_input(in_idx, [b_sz, 32, 32, 3])
 		model.allocate_tensors()
 		test_code = ''.join(('for batch in test_ds:\n',
 		' model.set_tensor(in_idx, batch)\n',
@@ -52,6 +56,13 @@ def main(args):
 		' prediction = model.get_tensor(out_idx)'))
 		test_vars = {'test_ds':test_ds, 'model':model, 
 				'in_idx':in_idx, 'out_idx':out_idx}
+	elif sav_mod: 
+		infer = model.signatures['serving_default']
+		output = infer.structured_outputs.keys()
+		output = list(output)[0]
+		test_code = ''.join(('for batch in test_ds:\n',
+		' prediction = infer(batch)[output]'))
+		test_vars = {'test_ds':test_ds, 'infer':infer, 'output':output}
 	else:
 		test_code = ''.join(('with device(dev):\n',
 		' for batch in test_ds:\n',
@@ -64,7 +75,7 @@ def main(args):
 	print('Metrics for model "%s", with batch size %d:' % (mod_file, b_sz))
 	print('Time: %.3f s' % time)
 	print('Speed: %.1f inf/s' % (steps * b_sz / time))
-	if args.acc and not tflite:
+	if args.acc and not tflite and not sav_mod:
 		print('Accuracy: %.2f' % (100 * acc), '%')
 	return 0
 
