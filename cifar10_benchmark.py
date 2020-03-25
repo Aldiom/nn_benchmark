cdcdb2bd142045360c52043f673d68182ea3fbe4
Aldiom <alexis.diomedi.12@sansano.usm.cl>
2020-03-25 01:40:54 -0300

    Agregado soporte para medir precisión de Saved Models. Ahora hay una única función que mide precisión para todo tipo de modelo.

--- cifar10_benchmark.py
+++ cifar10_benchmark.py
@@ -37,11 +37,12 @@ def main(args):
 		eval_ds = data.Dataset.zip((x_ds, y_ds))
 		print('Evaluating accuracy...')
 		if tflite:
-			acc = tflite_eval_acc(model, eval_ds)
-		elif not sav_mod:
-			model.compile(loss='sparse_categorical_crossentropy',
-              	metrics=['accuracy'])
-			_, acc = model.evaluate(eval_ds.batch(64), verbose=0)
+			mod_type = 'tflite'
+		elif sav_mod:
+			mod_type = 'saved'
+		else:
+			mod_type = 'keras'
+		acc = eval_accuracy(model, eval_ds, mod_type)	
 
 	test_ds = x_ds.take(1024).batch(b_sz)
 	steps = 1024 // b_sz
@@ -76,16 +77,30 @@ def main(args):
 	print('Metrics for model "%s", with batch size %d:' % (mod_file, b_sz))
 	print('Time: %.3f s' % time)
 	print('Speed: %.1f inf/s' % (steps * b_sz / time))
-	if args.acc and not sav_mod:
+	if args.acc:
 		print('Accuracy: %.2f' % (100 * acc), '%')
 	return 0
 
-def tflite_eval_acc(model, test_ds, in_shape=[32,32,3]):
+def eval_accuracy(model, test_ds, mod_type, in_shape=[32,32,3]):
 	test_ds = test_ds.batch(64)
-	in_idx = model.get_input_details()[0]['index'] 
-	out_idx = model.get_output_details()[0]['index'] 
-	model.resize_tensor_input(in_idx, [64] + in_shape)
-	model.allocate_tensors()
+
+	if mod_type == 'keras':
+		model.compile(loss='sparse_categorical_crossentropy',
+           	metrics=['accuracy'])
+		_, acc = model.evaluate(eval_ds, verbose=0)
+		return acc
+
+	if mod_type == 'tflite':
+		in_idx = model.get_input_details()[0]['index'] 
+		out_idx = model.get_output_details()[0]['index'] 
+		model.resize_tensor_input(in_idx, [64] + in_shape)
+		model.allocate_tensors()
+
+	if mod_type == 'saved':
+		infer = model.signatures['serving_default']
+		output = infer.structured_outputs.keys()
+		output = list(output)[0]
+
 	total_corrects = 0
 	total_examples = 0
 	for x_batch, y_batch in test_ds:
@@ -93,13 +108,20 @@ def tflite_eval_acc(model, test_ds, in_shape=[32,32,3]):
 		if b_sz != 64:
 			padding = ((0,64-b_sz), (0,0), (0,0), (0,0))
 			x_batch = pad(x_batch, padding)
-		model.set_tensor(in_idx, x_batch)
-		model.invoke()
-		outputs = model.get_tensor(out_idx)
+
+		if mod_type == 'tflite':	
+			model.set_tensor(in_idx, x_batch)
+			model.invoke()
+			outputs = model.get_tensor(out_idx)
+		elif mod_type == 'saved':
+			outputs = infer(x_batch)[output]
+			outputs = outputs.numpy()
+
 		outputs = outputs.argmax(axis=1).reshape((64,1))
 		corrects = outputs[0:b_sz] == y_batch.numpy()
 		total_corrects += corrects.sum()
 		total_examples += b_sz
+
 	return total_corrects / total_examples
 
 main(arguments)
