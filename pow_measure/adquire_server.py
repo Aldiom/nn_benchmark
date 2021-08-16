0af198d3a6ba81394efcace7a10786892eb44252
Aldiom <alexis.diomedi.12@sansano.usm.cl>
2021-08-16 16:59:58 -0400

    Update adquire_server.py

--- pow_measure/adquire_server.py
+++ pow_measure/adquire_server.py
@@ -61,7 +61,7 @@ dwf.FDwfAnalogInRecordLengthSet(hdwf, c_double(captureTime)) # -1 infinite recor
 # wait at least 2 seconds for the offset to stabilize
 time.sleep(2)
 
-HOST = '10.42.0.1'  # Standard loopback interface address (localhost)
+HOST = '192.168.0.10'  # Listen address, make sure to be on the same LAN
 PORT = 7700        # Port to listen on (non-privileged ports are > 1023)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
